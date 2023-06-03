864e5820fa08111663b8d214d15b78e542d2eba8
Jbarszcz02 <j.barszcz02@gmail.com>
2023-06-03 15:36:50 +0200

    zadania

---
+++ 1. Średnia arytmetyczna/1. Średnia arytmetyczna/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Wpisz trzy liczby:");
+
+        Double l1 = Convert.ToDouble(Console.ReadLine());
+        Double l2 = Convert.ToDouble(Console.ReadLine());
+        Double l3 = Convert.ToDouble(Console.ReadLine());
+
+        double sredniaaryt = (l1 + l2 + l3) / 3;
+        Console.WriteLine("Średnia arytmetyczna wynosi: " + sredniaaryt);
+    }
+}
---
+++ 2. Pole kola i obwod/2. Pole kola i obwod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pole koła:");
+
+        double Pole= Convert.ToDouble(Console.ReadLine());
+
+        double promien = Math.Sqrt(Pole / Math.PI);
+
+        double obwod = 2 * Math.PI * promien;
+
+        Console.WriteLine("Obwód koła wynosi: " + obwod);
+    }
+}
